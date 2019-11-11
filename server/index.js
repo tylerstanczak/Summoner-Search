4cfe0dea5cbfe8fee63f22cfb750b4f78c936aea
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-11-11 17:08:48 -0600

    use kayn to query riot api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const { Kayn, REGIONS } = require('kayn');
+const API_KEY = require('../key.js');
+const cors = require('cors');
 const port = 3000;
 
+const kayn = Kayn(API_KEY)();
 const app = express();
 
 app.use(express.static('dist'));
+app.use(cors());
+
+app.get('/search/:name', (req, res) => {
+  kayn.Summoner.by.name(req.params.name)
+    .then(result => {
+      res.send(result);
+    })
+    .catch(error => {
+      res.send(error);
+    })
+});
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);

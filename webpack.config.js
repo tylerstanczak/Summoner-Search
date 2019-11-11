93d8f895c30104dffa1e208b70c4d4ecdbf67c73
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-11-11 10:55:52 -0600

    creating some file structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/react']
+            }
+          }
+        ]
+      }
+    ]
+  }
+}

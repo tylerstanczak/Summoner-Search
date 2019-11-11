a7d4fb759f5a5f69ebda5871d42e94b1e358ab9b
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-11-11 11:12:38 -0600

    file structure

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
